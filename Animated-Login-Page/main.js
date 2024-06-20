741a6bbe56aede66d82c286e84ae4bc1a3f2ed64
nexgen9 <nexgenuse@gmail.com>
2024-06-20 21:01:06 +0200

    add new project

---
+++ Animated-Login-Page/main.js
@@ -0,0 +1,20 @@
+const registerBtn=document.getElementById("register")
+const container=document.getElementById("container")
+const loginBtn=document.getElementById("login")
+registerBtn.addEventListener("click",()=>{
+    container.classList.add("active")
+})
+loginBtn.addEventListener("click",()=>{
+    container.classList.remove("active")
+})
+
+
+
+
+
+
+
+
+
+
+
